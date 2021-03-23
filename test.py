ff48e9ea4ee60cd91c6b658cf7a43c7d3b7e1342
Abitamim <abitamimbharmal@hotmail.com>
2021-03-22 20:42:57 -0500

    test 12

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os 
 f = open("potato.txt", 'w+')
-f.write(f"running test 11 in {os.listdir()}")
-
-print("test number 11 I think")
+f.write(f"running test 12 in {os.listdir()}")
+f.close()
+print(str(os.listdir())+"test number 12 I think")
